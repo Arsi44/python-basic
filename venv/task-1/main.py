1b3d212e5007031f8d38e804377a398063f91fea
Ars44 <Hrieleght2>
2021-06-18 16:29:49 +0300

    add power

--- venv/task-1/main.py
+++ venv/task-1/main.py
@@ -1,19 +1,18 @@
 
 
 # возвращаем список результатов во 2ой степени
-def square_func(*args):
+def square_func(power, *args):
     all_results = []
     for arg in args:
-        all_results.append(arg**2)
+        all_results.append(arg**power)
     return all_results
 
-# возвращаем генератор
-def square_func_gen(*args):
-    return (arg**2 for arg in args)
+# # возвращаем генератор
+# def square_func_gen(*args):
+#     return (arg**2 for arg in args)
+# x_2 = square_func_gen(2, 3, 4)
 
+x_1 = square_func(3, 2, 3, 4)
 
-x_1 = square_func(2, 3, 4)
-x_2 = square_func_gen(2, 3, 4)
 
 print(x_1)
-print(x_2)
