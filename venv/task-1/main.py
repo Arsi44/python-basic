0b706ac897ef79ca2eefe72172a2de02cc0bcc65
Ars44 <Hrieleght2>
2021-06-18 16:44:19 +0300

    add annotations add second function

--- venv/task-1/main.py
+++ venv/task-1/main.py
@@ -1,18 +1,30 @@
-
-
+# 1.
 # возвращаем список результатов во 2ой степени
-def square_func(power, *args):
+def square_func(power, *args) -> list:
     all_results = []
     for arg in args:
-        all_results.append(arg**power)
+        all_results.append(arg ** power)
     return all_results
+
+
+x_1 = square_func(3, 2, 3, 4)
+
 
 # # возвращаем генератор
 # def square_func_gen(*args):
 #     return (arg**2 for arg in args)
 # x_2 = square_func_gen(2, 3, 4)
+##################################################
 
-x_1 = square_func(3, 2, 3, 4)
+# 2.
+# возвращаем четные/нечетные числа из списка
+def get_numbers(nums: list, parity: bool) -> list:
+    if parity:
+        result_nums = [num for num in nums if num % 2 == 0]
+    else:
+        result_nums = [num for num in nums if num % 2 != 0]
+    return result_nums
 
 
-print(x_1)
+lst_get_numbers = get_numbers([2, 3, 4, 5, 6, 90, 91], parity = False)
+print(lst_get_numbers)
